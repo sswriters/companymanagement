66fb5fb468f776a67e7480b48477e14c824d4e81
aasd asfsaf <aaannniiilll@hotmail.com>
2018-12-06 01:33:25 +0300

    git add *

---
+++ Web Project/CompanyManagementSystem/CompanyManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CompanyManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
