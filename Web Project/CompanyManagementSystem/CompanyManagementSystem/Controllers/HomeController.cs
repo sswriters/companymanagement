401e88c4109877bbd9ba4b25724bf1de0790fa2d
aasd asfsaf <aaannniiilll@hotmail.com>
2018-12-06 11:31:53 +0300

    interfaces complated

--- Web Project/CompanyManagementSystem/CompanyManagementSystem/Controllers/HomeController.cs
+++ Web Project/CompanyManagementSystem/CompanyManagementSystem/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using CompanyManagementSystem.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyManagementSystem.Controllers
 {
@@ -25,22 +19,16 @@ namespace CompanyManagementSystem.Controllers
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult Customer_Index()
         {
-            ViewData["Message"] = "Your contact page.";
 
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Customer_Info()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- Web Project/CompanyManagementSystem/CompanyManagementSystem/Models/ErrorViewModel.cs
+++ Web Project/CompanyManagementSystem/CompanyManagementSystem/Models/ErrorViewModel.cs
@@ -1,6 +1,6 @@
-using System;
+﻿using System;
 
-namespace CompanyManagementSystem.Models
+namespace CompanyManagementsystem.Models
 {
     public class ErrorViewModel
     {
